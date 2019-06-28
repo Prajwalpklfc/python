6c0e0d268bffb1cbf276aaf5179f2a414ed59cd7
prajwal <prajwalpsh99@gmail.com>
2019-06-28 13:08:44 +0530

    first commit

---
+++ view.py
@@ -0,0 +1,15 @@
+import pymysql
+def view(id1):
+        myconn = pymysql.connect(host = "localhost", user = "root",passwd = "170905044",database="python")
+        str1="select * from employee where id ="+id1
+        cur = myconn.cursor()
+        try:
+            db = cur.execute(str1)
+            if db==0:
+                print("No such record exists")
+                return
+        except:
+           print("error")
+        for x in cur:
+            print(x)
+
